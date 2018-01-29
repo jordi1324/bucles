e6e1c933cffd9eda2640c593c2f0b8cd4c05453c
root <root@localhost.localdomain>
2018-01-29 13:46:20 +0100

    first commit

---
+++ bucle3.py
@@ -0,0 +1,9 @@
+num1=int(input("Escriu un nombre: "))
+num2=int(input("Escriu un altre: "))
+while (num1<num2):
+        num1=num2
+        num2=int(input("Escriu un nombre major que:  "))
+print("No es mes gran")
+    
+  
+    
---
+++ bucle5.py
@@ -0,0 +1,8 @@
+num=int(input("Escribe un numero: "))
+num1=int(input("Escribe otro: "))
+
+while (num>0 and num1>0):
+    num=num1
+    num=int(input("Escribe otro: "))
+
+print("No puede ser negativo")
