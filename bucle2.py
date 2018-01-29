ecdcc8a55833912d71e8dc85c9703b6e7b643c78
root <root@localhost.localdomain>
2018-01-29 12:56:56 +0100

    first commit

---
+++ bucle2.py
@@ -0,0 +1,9 @@
+num1=float(input("Escriu un nombre: "))
+num2=float(input("Escriu un nombre major que el anterior: "))
+num=0
+while (num1>num2):
+    print(num2,"No es mes gran que ",num1)
+    num2=int(input("Escriu un altre "))
+
+print("Els nombres que has escrit són ", num1  ,  num2)
+num=+1
