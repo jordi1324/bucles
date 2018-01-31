0f7ed12258ab83d22138db43bb7d549fea511cbc
root <root@localhost.localdomain>
2018-01-31 11:38:04 +0100

    first commit

---
+++ bucle_1.py
@@ -0,0 +1,7 @@
+num=int(input("Escriu un nombre: "))
+i=0
+
+while (i<num):
+    i=i+1
+    print("Introdueix un nombre")#Canvia el interior del parentesis depenen del que vulguis imprimir n vegades.
+    
